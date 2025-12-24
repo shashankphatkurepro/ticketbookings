a864b73eb43c8c23615445d91a108ebd2733bafd
shashankphatkure <shashankphatkure@gmail.com>
2025-12-24 17:25:16 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { BookingProvider } from "./context/BookingContext";
+import WhatsAppButton from "./components/WhatsAppButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <BookingProvider>
           {children}
+          <WhatsAppButton />
         </BookingProvider>
       </body>
     </html>
